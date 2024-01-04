420f4c5aeb216ec712fe703eb335af02671d72a2
Honse <vasavj123@gmail.com>
2024-01-04 17:35:03 +0200

    added controls, fixed some endpoints, almost done. still cant figure out loading

--- slideshow/app/[slug]/page.tsx
+++ slideshow/app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Display from '@/components/Display';
 import React, {useState} from 'react'
-import useSWR from 'swr'
+import useSWRImmutable from 'swr'
 const snoowrap = require('snoowrap');
 
 const bots = new snoowrap({
@@ -12,20 +12,28 @@ const bots = new snoowrap({
 });
 
 const fetcher = async (url:string) => fetch(url).then((res) => {
+  console.log(1);
     return res.json()
 } );
+
+const domains = ['self.nsfw', 'youtube.com','i.redgifs.com'];
 
 function page({params }: {params:{slug: string}}) {
 
     const [number, setNumber] = useState(0);
     const [showTitle, changeTitle] = useState(true);
 
-    const { data, error, isLoading } = useSWR(`/${params.slug}/api`,fetcher);
-    
+    const { data, error, isLoading } = useSWRImmutable(`/${params.slug}/api`,fetcher);
+     
+    const setter = () => {
+      setNumber(preVal => preVal>0 ? preVal-1 : preVal+0);
+      return (
+        <div className='h-screen bg-black'></div>
+      )
+    }
       if (error) return "An error has occurred.";
       if (isLoading) return "Loading...";
-      console.log(data);
-      
+
       if(data?.posts === undefined) return (
         <div className='flex flex-col bg-black items-center justify-center h-screen'>
           <div className='flex flex-col bg-black items-center justify-center'>
@@ -39,23 +47,55 @@ function page({params }: {params:{slug: string}}) {
         </div>
       );
 
+      //@ts-ignore
+      const filter = data?.posts.filter((obj)  => {
+        return domains.includes(obj?.domain) === false;
+      })
+
+      const filterData = {
+        posts: filter
+      };
 
       return (
-        <div className='h-svh'>
-          {showTitle ? <div className=' z-50 absolute'>
-                <button className='z-50 ' type='button' onClick={() => {setNumber(preCount => preCount+=1)}}> click me</button>
-            <p className='z-10'>{data?.posts[number]?.title}</p>
-            <button type='button' className='absolute' onClick={() => changeTitle(preVal => preVal ? false : true) }>+</button>
-            </div> : <button type='button' className='absolute' onClick={() => changeTitle(preVal => preVal ? false : true) }>+</button> }
-            
-            
-            
-           <div className='flex flex-col h-screen items-center p-0 m-0'>
-           <Display data={data} num={number} />
+        <div className='h-svh bg-black'>
+          {showTitle ? <div className='h-14 z-50 absolute bg-black opacity-70'>
+            <p className='z-10 p-1 pl-2 pr-2 text-lg'>{filterData?.posts[number]?.title}</p>
 
-          </div> 
+            <button type='button' className='absolute right-1 bottom-0 text-xl' onClick={() => changeTitle(preVal => preVal ? false : true) }>+</button>
+            </div> : 
+            <button type='button' className='absolute z-50 text-xl' onClick={() => changeTitle(preVal => preVal ? false : true) }>+</button>
+             }
             
-        </div>
+            <div className='fixed top-80 z-40'>
+              <button type='button' onClick={() => setNumber(preVal => preVal>0 ? preVal-1 : preVal+0)} className='z-50 absolute'>
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+             </svg>
+
+           </button>
+            </div>
+
+            <div className='fixed top-80 z-40 right-12'>
+              <button type='button' onClick={() => setNumber(preVal => preVal<filterData.posts.length ? preVal+1 : preVal+0)} className='z-50 absolute'>
+
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+              </svg>
+
+           </button>
+            </div>
+            
+            
+           <div className='flex flex-col h-screen items-center bg-black p-0 m-0'>
+           <Display data={filterData} num={number} />
+           </div>
+
+           <div>
+
+           </div>
+          </div> 
+
       );
 }
 
--- slideshow/components/Display.tsx
+++ slideshow/components/Display.tsx
@@ -1,4 +1,5 @@
-import React, { Dispatch, SetStateAction } from 'react'
+'use client'
+import React, {useState} from 'react';
 
 interface props {
     data: {posts: [any]};
@@ -6,28 +7,74 @@ interface props {
 
 }
 
+
+
 function Display(props: props) {
-    
-    if(props.data?.posts[props.num]?.domain === 'reddit.com') {
-        return (
-            <h1>sorry</h1>
-        )
+    const [loading, setBool] = useState(false);
+    console.log(props.data)
+    console.log(props.num);
+    //setBool(true);
+
+    const displayer = () => {
+        if(loading) {
+            return <div>loading</div>
+        }
+
+        if(props.data?.posts[props.num]?.domain.includes('redgifs.com') ) {
+            return (
+                <iframe allowFullScreen className='h-svh w-svw'src={props.data?.posts[props.num]?.url.replace("watch",'ifr')} />
+            )
+        }
+        
+        else if(props.data?.posts[props.num]?.media !== null) {
+            return (
+                <div className='bg-black h-screen w-screen flex flex-col items-center'>
+                <video className='h-svh w-svw' autoPlay controls loop src={props.data.posts[props.num]?.media?.reddit_video?.fallback_url || props.data.posts[props.num]?.url}></video>
+                </div>
+            )
+        }
+
+        else if(props.data?.posts[props.num]?.domain === 'reddit.com') {
+            if(props.data?.posts[props.num]?.gallery_data?.items[0]?.media_id && props.data?.posts[props.num]?.media_metadata !== null) {
+                const val  = props.data?.posts[props.num]?.gallery_data?.items[0]?.media_id
+                return(
+                    <img className='h-svh' src={props.data?.posts[props.num]?.media_metadata[val].s?.u}></img> 
+                )
+            }
+            else{
+                return (
+                    <img className='h-svh' src={props.data.posts[props.num]?.url}></img>
+                )
+            }
+        }
+
+        else if (!props.data?.posts[props.num]?.preview?.enabled){
+            return(
+                <div className='bg-black h-screen w-screen flex flex-col items-center'>
+                <video  className='h-svh w-svw' autoPlay controls loop src={props.data?.posts[props.num]?.preview?.reddit_video_preview?.fallback_url}></video>
+                </div>
+            )
+        }
+        
+        else {
+
+            return (
+                    <div className='bg-black h-screen w-screen flex flex-col items-center'>
+                    <img className='h-svh' src={props.data.posts[props.num]?.url}></img> 
+                    </div>
+            
+                
+            )
     }
-    else if(props.data?.posts[props.num].media !== null) {
-        return (
-            <video className='h-svh w-svw' autoPlay controls loop src={props.data.posts[props.num].media?.reddit_video?.fallback_url || props.data.posts[props.num].url}></video>
-        )
     }
-    else if (!props.data?.posts[props.num]?.preview.enabled){
-        return(
-            <video className='h-svh w-svw' autoPlay controls loop src={props.data?.posts[props.num]?.preview.reddit_video_preview?.fallback_url}></video>
-        )
-    }
-    else {
-        return (
-            <img className='h-svh' src={props.data.posts[props.num].url}></img>
-        )
-}
+    //setBool(false);
+
+    return (
+        <div className='flex flex-col h-screen bg-black'>
+            {displayer()}
+        </div>
+    )
+
 }
 
 export default Display
